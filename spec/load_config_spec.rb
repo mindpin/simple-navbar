d0f66d50ec24713d911fac6d44582666476c1f1c
fushang318 <fushang318@gmail.com>
2013-03-12 12:29:59 +0800

    change rspec

--- spec/load_config_spec.rb
+++ spec/load_config_spec.rb
@@ -27,87 +27,180 @@ describe "读取配置相关" do
       require 'config/simple_navbar_config'
     }
 
-    it 'simple' do
-      rule = SimpleNavbar::Rule.get(:simple)
-      rule.title.should == :simple
+    describe 'simple' do
+      before(:all){
+        @rule = SimpleNavbar::Rule.get(:simple)
+      }
 
-      navs = rule.navs
-      navs.length.should == 4
+      it{
+        @rule.title.should == :simple
+      }
 
-      index_nav = navs[0]
-      index_nav.title.should == :index
-      index_nav.options[:url].should == '/'
-      index_nav.options[:name].should == '首页'
-      controller_items = index_nav.controller_items
-      controller_items.length.should == 1
-      controller_item = controller_items[0]
-      controller_item.controller_name.should == :index
+      it{
+        @rule.navs.length.should == 4
+      }
 
-      books_nav = navs[1]
-      books_nav.title.should == :books
-      movies_nav = navs[2]
-      movies_nav.title.should == :movies
-      musics_nav = navs[3]
-      musics_nav.title.should == :musics
-    end
+      it{
+        @rule.navs[0].title.should == :index
+      }
 
-    it 'multi_level_example' do
-      rule = SimpleNavbar::Rule.get(:multi_level_example)
-      rule.title.should == :multi_level_example
-      
-      navs = rule.navs
-      navs.length.should == 3
+      it{
+        @rule.navs[0].options[:url].should == '/'
+      }
 
-      musics_nav = navs[2]
-      controller_items = musics_nav.controller_items
-      controller_items.length.should == 4
-      controller_items[0].controller_name.should == :musics
-      controller_items[1].controller_name.should == :pop_musics
-      controller_items[2].controller_name.should == :rock_musics
-      controller_items[3].controller_name.should == :punk_musics 
+      it{
+        @rule.navs[0].options[:name].should == '首页'
+      }
 
-      musics_nav.subnavs.length.should == 1
-      pop_nav = musics_nav.subnavs[0]
-      controller_items = pop_nav.controller_items
-      controller_items.length == 3
-      controller_items[0].controller_name.should == :pop_musics
-      controller_items[1].controller_name.should == :rock_musics
-      controller_items[2].controller_name.should == :punk_musics 
+      it{
+        controller_items = @rule.navs[0].controller_items
+        controller_items.length.should == 1
+      }
 
-      pop_nav.subnavs.length.should == 1
-      rock_nav = pop_nav.subnavs[0]
-      controller_items = rock_nav.controller_items
-      controller_items.length == 2
-      controller_items[0].controller_name.should == :rock_musics
-      controller_items[1].controller_name.should == :punk_musics
+      it{
+        controller_item = @rule.navs[0].controller_items[0]
+        controller_item.controller_name.should == :index
+      }
 
-      rock_nav.subnavs.length.should == 1
-      punk_nav = rock_nav.subnavs[0]
-      controller_items = punk_nav.controller_items
-      controller_items.length == 1
-      controller_items[0].controller_name.should == :punk_musics
-      
+      it{
+        @rule.navs[1].title.should == :books
+      }
+
+      it{
+        @rule.navs[2].title.should == :movies
+      }
+
+      it{
+        @rule.navs[3].title.should == :musics
+      }
 
     end
 
-    it 'mutil_rule' do
-      rule_1 = SimpleNavbar::Rule.get(:rule_1)
-      rule_1.title.should == :rule_1
+    describe 'multi_level_example' do
+      before(:all){
+        @rule = SimpleNavbar::Rule.get(:multi_level_example)
+        @rock_nav = @rule.navs[2].subnavs[0].subnavs[0]
+        @pop_controller_items = @rule.navs[2].subnavs[0].controller_items
+      }
 
-      rule_2 = SimpleNavbar::Rule.get(:rule_1)
-      rule_2.title.should == :rule_1
+      it{
+        @rule.title.should == :multi_level_example  
+      }
 
-      rule_1.navs.length == 4
-      rule_1.navs.length == rule_2.navs.length
+      it{
+        @rule.navs.length.should == 3
+      }
+
+      it{
+        @rule.navs[2].controller_items.length.should == 4
+      }
+
+      it{
+        @rule.navs[2].controller_items[0].controller_name.should == :musics
+      }
+
+      it{
+        @rule.navs[2].controller_items[1].controller_name.should == :pop_musics
+      }
+
+      it{
+        @rule.navs[2].controller_items[2].controller_name.should == :rock_musics
+      }
+
+      it{
+        @rule.navs[2].controller_items[3].controller_name.should == :punk_musics 
+      }
+
+      it{
+        @rule.navs[2].subnavs.length.should == 1
+      }
+
+      it{
+        @rule.navs[2].subnavs[0].controller_items.length == 3
+      }
+
+      it{
+        @pop_controller_items[0].controller_name.should == :pop_musics
+      }
+
+      it{
+        @pop_controller_items[1].controller_name.should == :rock_musics
+      }
+
+      it{
+        @pop_controller_items[2].controller_name.should == :punk_musics 
+      }
+
+      it{
+        @rule.navs[2].subnavs[0].subnavs.length.should == 1
+      }
+
+      it{
+        @rock_nav.controller_items.length == 2
+      }
+
+      it{
+        @rock_nav.controller_items[0].controller_name.should == :rock_musics
+      }
+
+      it{
+        @rock_nav.controller_items[1].controller_name.should == :punk_musics
+      }
+
+      it{
+        @rock_nav.subnavs.length.should == 1  
+      }
+
+      it{
+        @rock_nav.subnavs[0].controller_items.length == 1
+      }
+
+      it{
+        @rock_nav.subnavs[0].controller_items[0].controller_name.should == :punk_musics
+      }
+
     end
 
-    it 'admin' do
-      rule_admin = SimpleNavbar::Rule.get(:admin)
-      nav = rule_admin.navs[0]
+    describe 'mutil_rule' do
+      before(:all){
+        @rule_1 = SimpleNavbar::Rule.get(:rule_1)
+        @rule_2 = SimpleNavbar::Rule.get(:rule_2)
+      }
 
-      nav.title.should == :index
-      nav.controller_items.length == 1
-      nav.controller_items[0].controller_name.should == :'admin/index'
+      it{
+        @rule_1.title.should == :rule_1  
+      }
+
+      it{
+        @rule_2.title.should == :rule_2
+      }
+
+      it{
+        @rule_1.navs.length == 4
+      }
+
+      it{
+        @rule_1.navs.length == @rule_2.navs.length
+      }
+    end
+
+    describe 'admin' do
+      before(:all){
+        @rule_admin = SimpleNavbar::Rule.get(:admin)
+        @nav = @rule_admin.navs[0]
+      }
+
+      it{
+        @nav.title.should == :index
+      }
+
+      it{
+        @nav.controller_items.length == 1  
+      }
+
+      it{
+        @nav.controller_items[0].controller_name.should == :'admin/index'
+      }
     end
 
   end
@@ -115,8 +208,6 @@ describe "读取配置相关" do
   describe '#simple_navbar' do
     before(:all) {
       html_str = MOCK_VIEW.simple_navbar(:simple)
-      puts html_str
-
       @xml = Nokogiri::XML(html_str)
     }
 
