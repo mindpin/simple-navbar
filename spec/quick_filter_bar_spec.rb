906d9fb881fb9d85ffb3c5446d8de55ffc327719
fushang318 <fushang318@gmail.com>
2015-12-02 15:59:37 +0800

    fix #5

--- spec/quick_filter_bar_spec.rb
+++ spec/quick_filter_bar_spec.rb
@@ -63,4 +63,45 @@ describe "quick_filter_bar" do
       a["href"].should == "/list?kind=pop&lang=english"
       a.content.should == "英语"
     }
+
+    describe "fix bug /list?a=1&kind=" do
+      before(:all){
+        @view = MOCK_VIEW
+
+        def @view.params
+          {
+            :kind => "",
+            :a    => "1"
+          }
+        end
+
+        def @view.request
+          request = ActionDispatch::Request.new({})
+          def request.fullpath
+            "/list?a=1&kind="
+          end
+          request
+        end
+
+        html = @view.quick_filter_bar do |builder|
+          builder.group :kind, :text => "音乐类型" do |group|
+            group.add "rock",   :text => "摇滚"
+            group.add "pop", :text => "流行"
+          end
+
+          builder.group :lang, :text => "语种" do |group|
+            group.add "china",   :text => "华语"
+            group.add "english", :text => "英语"
+          end
+        end
+
+        @xml = Nokogiri::XML(html)
+      }
+
+      it {
+        a = @xml.at_css(".quick_filter_bar .quick_filter_bar_group:last-child ul li:first-child a")
+        a["href"].should == "/list?a=1&kind="
+        a.content.should == "全部"
+      }
+    end
 end
